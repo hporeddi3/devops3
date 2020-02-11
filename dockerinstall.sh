45663e6878f303843182646b4931648626a7c387
root <root@poreddiharsha2c.mylabserver.com>
2020-02-11 11:58:44 -0500

    first commit

--- dockerinstall.sh
+++ dockerinstall.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
-
-sudo tee /etc/yum.repos.d/docker.repo <<-'EOF'
- [dockerrepo]
- name=Docker Repository
- baseurl=https://yum.dockerproject.org/repo/main/centos/7/
- enabled=1
- gpgcheck=1
- gpgkey=https://yum.dockerproject.org/gpg
- EOF
-sudo yum install docker-engine -y;
-
+curl -fsSL https://get.docker.com | sh;
 systemctl enable docker;systemctl restart docker
 docker pull httpd:latest
 docker run -d --name httpd 8081:80 httpd:latest
